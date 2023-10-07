149c389a666d608dfd101f69263c9becf4b5d7bb
sahosridoy <sahosmia.webdev@gmail.com>
2023-10-07 13:06:33 +0600

    blog post api add

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/about",
-        destination: "/",
+        source: "/api/:path*",
+        destination: "https://api.example.com/:path*",
       },
     ];
   },
